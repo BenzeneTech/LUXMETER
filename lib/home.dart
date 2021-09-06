dc029b4bb1573e766ecfdd6b19735574d08ed059
Akshit Madan <akmadan1717@gmail.com>
2021-09-06 10:41:19 +0530

    circular chart added

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:light/light.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,9 +12,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  String _luxString = 'Unknown';
+  String _luxString = '20';
   late Light _light;
   late StreamSubscription _subscription;
+  var chartData = [
+    _ChartData('Akshit', 90000, Colors.red),
+  ];
 
   void onData(int luxValue) async {
     print("Lux value: $luxValue");
@@ -48,23 +52,73 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: new Scaffold(
-        // appBar: new AppBar(
-        //   title: const Text('Light Example App'),
-        // ),
-        body: new Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '$_luxString',
-                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
+    return Scaffold(
+      // backgroundColor: Colors.black87,
+      // appBar: new AppBar(
+      //   title: const Text('Light Example App'),
+      // ),
+      body: new Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('$_luxString'),
+            Container(
+                child: SfCircularChart(
+                    // backgroundColor: Colors.black,
+                    series: <CircularSeries>[
+                  // RADIAL | DOUGHNUT SERIES
+                  RadialBarSeries<_ChartData, String>(
+                      maximumValue: 100,
+                      // trackColor: Colors.grey.shade800,
+                      dataSource: chartData,
+                      xValueMapper: (_ChartData data, _) => '',
+                      yValueMapper: (_ChartData data, _) =>
+                          num.parse(_luxString),
+                      pointColorMapper: (_ChartData data, _) => data.color,
+                      cornerStyle: CornerStyle.bothCurve,
+                      dataLabelSettings: DataLabelSettings(isVisible: true),
+                      radius: '100%',
+                      innerRadius: '80%'),
+                ])),
+            //      Container(
+            //   child: SfRadialGauge(axes: <RadialAxis>[
+            //     RadialAxis(
+            //         annotations: <GaugeAnnotation>[
+            //           GaugeAnnotation(
+            //               widget: Container(
+            //                   child: Text('$_luxString',
+            //                       style: TextStyle(
+            //                           fontSize: 25,
+            //                           fontWeight: FontWeight.bold))),
+            //               angle: 90,
+            //               positionFactor: 0.5)
+            //         ],
+            //         startAngle: 270,
+            //         endAngle: 270,
+            //         axisLineStyle: AxisLineStyle(
+            //           thickness: 0.1,
+            //           thicknessUnit: GaugeSizeUnit.factor,
+            //           gradient: const SweepGradient(
+            //             colors: <Color>[Colors.yellow, Colors.amber],
+            //             // stops: <double>[0.25, 0.75]
+            //           ),
+            //         )),
+            //   ]),
+            // ),
+            // Text(
+            //   '$_luxString',
+            //   style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            // )
+          ],
         ),
       ),
     );
   }
+}
+
+class _ChartData {
+  _ChartData(this.x, this.y, this.color);
+  final String x;
+  final num y;
+  final Color color;
 }
